64c1118b50a15c6468d0b4697e7a44ff82c2b516
--global <--global>
2026-06-22 19:29:50 +0900

    add lesson 8

---
+++ lesson_08/include/robot_controller.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+class RobotController
+{
+public:
+    RobotController(
+        const std::string & robot_name,
+        double kp,
+        double min_limit,
+        double max_limit);
+
+    void printInfo();
+
+    bool isInsideLimit(double target_position);
+
+    double computeError(
+        double target_position,
+        double current_position);
+
+    double computeCommand(double error);
+
+private:
+    std::string robot_name_;
+    double kp_;
+    double min_limit_;
+    double max_limit_;
+};
