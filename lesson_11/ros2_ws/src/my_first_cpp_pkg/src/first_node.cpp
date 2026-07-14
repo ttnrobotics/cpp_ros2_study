8cf02ce42c986a9220422e805f2fe455773fcfd6
ttnrobotics <truong.thanhnguyen.robotics@gmail.com>
2026-07-14 18:51:56 +0900

    add first node

--- lesson_11/ros2_ws/src/my_first_cpp_pkg/src/first_node.cpp
+++ lesson_11/ros2_ws/src/my_first_cpp_pkg/src/first_node.cpp
@@ -6,19 +6,35 @@
 using namespace std::chrono_literals;
 
 
-class 
+class FirstRobotNode : public rclcpp::Node
 {
-private:
-    /* data */
 public:
-    (/* args */);
-    ~();
+    FirstRobotNode() : Node("first_robot_node")
+    {
+        target_position_ = 1.57;
+        current_position_ = 0.0;
+
+        timer_ = this->create_wall_timer(1000ms,
+                                        std::bind(&FirstRobotNode::controlLoop, this);
+        RCLCPP_INFO(this->get_logger(), "First ROS2 C++ node started.");
+    }
+private:
+    void controlLoop()
+    {
+        double error = target_position_ - current_position_;
+        current_position_ += 0.1 * error;
+
+        RCLCPP_INFO(
+            this->get_logger(),
+            "target = %.3f, current = %.3f, error = %.3f",
+            target_position_,
+            current_position_,
+            error);
+    }
+
+    double target_position_;
+    double current_position_;
+
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
-::(/* args */)
-{
-}
-
-::~()
-{
-}
