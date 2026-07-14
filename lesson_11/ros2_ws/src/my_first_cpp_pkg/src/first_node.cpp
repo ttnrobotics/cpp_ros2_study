2818bed24698476148cdba5551342a0772d599c5
ttnrobotics <truong.thanhnguyen.robotics@gmail.com>
2026-07-14 17:03:46 +0900

    add lesson 11

--- lesson_11/ros2_ws/src/my_first_cpp_pkg/src/first_node.cpp
+++ lesson_11/ros2_ws/src/my_first_cpp_pkg/src/first_node.cpp
@@ -1,1 +1,24 @@
 #include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+
+using namespace std::chrono_literals;
+
+
+class 
+{
+private:
+    /* data */
+public:
+    (/* args */);
+    ~();
+};
+
+::(/* args */)
+{
+}
+
+::~()
+{
+}
